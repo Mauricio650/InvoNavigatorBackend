a59b5b5565f2d09eb2d4b231a7485603a3a02802
Mauricio650 <mauricioibanez650@gmail.com>
2025-08-14 22:50:55 -0500

    optimizations deleteFileGridfs

--- InvoiceNavigator/src/config/db.js
+++ InvoiceNavigator/src/config/db.js
@@ -16,13 +16,30 @@ export const db = mongoose.connect(url, {
 const dataBase = mongoose.connection
 
 // delete files
-export const deleteFile = async ({ fileId }) => {
+/* export const deleteFile = async ({ fileId }) => {
   try {
     const bucket = new GridFSBucket(dataBase.db, { bucketName: 'invoices' })
     const id = mongoose.Types.ObjectId(fileId)
     await bucket.delete(id)
   } catch (error) {
     console.log('Error deleting invoice', error)
+  }
+} */
+
+export const deleteFile = async ({ fileId }) => {
+  try {
+    const bucket = new GridFSBucket(dataBase.db, { bucketName: 'invoices' })
+    const id = new mongoose.Types.ObjectId(fileId)
+
+    const fileExists = await bucket.find({ _id: id }).hasNext()
+
+    if (fileExists) {
+      await bucket.delete(id)
+    } else {
+      console.warn(`file with id ${fileId} not found in GridFS`)
+    }
+  } catch (error) {
+    console.log('Error deleting invoice file', error)
   }
 }
 
