d21f9aca9d72ccf627c53742fc1909ce25d921cb
Mauricio650 <mauricioibanez650@gmail.com>
2025-08-17 20:26:46 -0500

    add cors with vercel

--- InvoiceNavigator/src/cors/cors.js
+++ InvoiceNavigator/src/cors/cors.js
@@ -1,5 +1,5 @@
 export const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://invo-navigator.vercel.app'],
   methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
